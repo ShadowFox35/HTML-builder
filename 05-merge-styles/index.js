3ee4539ae99a557d8d3ceab2fd77923f2134960f
ShadowFox35 <94312514+ShadowFox35@users.noreply.github.com>
2022-05-23 01:24:45 +0300

    feat: solution 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const currentFolder = path.resolve(__dirname, 'styles');
+const finalFolder = path.resolve(__dirname, 'project-dist');
+
+const writeStream = fs.createWriteStream(
+  path.resolve(finalFolder, 'bundle.css')
+);
+
+async function createBundle() {
+  const folder = await fs.promises.readdir(currentFolder, {
+    withFileTypes: true,
+  });
+
+  folder.forEach((file) => {
+    if (
+      file.isFile() &&
+      path.extname(
+        path.join(currentFolder, `${file.name}`)
+      ) === '.css'
+    ) {
+      const readStream = fs.createReadStream(
+        path.resolve(currentFolder, file.name)
+      );
+      readStream.pipe(writeStream, { end: false });
+    }
+  });
+}
+
+createBundle();
