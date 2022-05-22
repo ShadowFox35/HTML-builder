6fd63e205106c9ea60ccab1387c1301721db65cb
ShadowFox35 <94312514+ShadowFox35@users.noreply.github.com>
2022-05-22 16:08:37 +0300

    feat: solution 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(
+  path.join(__dirname, './secret-folder'),
+  (err, folder) => {
+    for (let file of folder) {
+      fs.stat(
+        path.join(__dirname, './secret-folder', file),
+        (err, stats) => {
+          if (stats.isFile()) {
+            const fileName = path.basename(
+              file,
+              path.extname(file)
+            );
+            const fileExtension = path
+              .extname(file)
+              .substring(1);
+            const fileSize = stats.size / 1000;
+
+            process.stdout.write(
+              `${fileName} - ${fileExtension} - ${fileSize}kb\n`
+            );
+          }
+        }
+      );
+    }
+  }
+);
